bbcb14f10a2b4ad8c4d1ab0f67a9776b1f7851cf
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-11-26 21:21:40 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ arguments
 -i: input file path(must be a jpeg file)
 -o: output file path(will be a midi file)
 -m: convert mode('gate' or 'edge')
+-s: show image when converting
 -y, -n: overwrite confirm(to overwrite or not)
 -h: help(see all of the arguments)
 '''
@@ -35,6 +36,7 @@ argTypes = {
     'input': ['i', 'input', 'image', 'img'], 
     'output': ['o', 'output', 'midi', 'mid'], 
     'mode': ['m', 'mode', 'mod'], 
+    'show': ['w', 'show', 'window', 'win'], 
     'yes': ['y', 'yes', 'skip'], 
     'no': ['n', 'no', 'dontskip'], 
     'help': ['h', 'help', 'description', 'des']
@@ -52,6 +54,7 @@ if 'help' in args:
     print(des)
 if 'input' not in args:
     exit()
+show = 'show' in args and args['show']
 imagePath = path + '/image2midi_input.jpg'
 inputPath = args['input'] if 'input' in args else path + '/input.jpg'
 midiPath = path + '/image2midi_output.mid'
@@ -65,8 +68,9 @@ widthPerHeight = width/height
 
 dots = [[image[r*int(height/row)][c*int(height/row)] for c in range(int(row*widthPerHeight))] for r in range(row)]
 dotsImage = numpy.array(dots)
-cv2.imshow('dotsImage', dotsImage)
-cv2.waitKey(1)
+if show:
+    cv2.imshow('dotsImage', dotsImage)
+    cv2.waitKey(1)
 
 if 'mode' in args:
     if args['mode'] in ['default', 'fill', 'gate']: pass
@@ -74,8 +78,9 @@ if 'mode' in args:
         dotsImage = cv2.cvtColor(dotsImage, cv2.COLOR_BGR2GRAY)
         dotsImage = cv2.medianBlur(dotsImage, 7)
         dotsImage = cv2.Laplacian(dotsImage, -1, 1, 5)
-        cv2.imshow('dotsImageLaplacian', dotsImage)
-        cv2.waitKey(1)
+        if show:
+            cv2.imshow('dotsImageLaplacian', dotsImage)
+            cv2.waitKey(1)
 
 dots = [[(c if type(c) == int else sum(c)/3) > 255/2 for c in r] for r in dotsImage.tolist()]
 
@@ -88,35 +93,41 @@ midi = mido.MidiFile()
 track = mido.MidiTrack()
 midi.tracks.append(track)
 bpm = 240
+track.append(mido.MetaMessage('text', text='convert by image2midi(MaoHuPi)'))
+track.append(mido.MetaMessage('track_name', name=inputPath.replace('\\', '/').split('/')[-1]))
+track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(bpm)))
 # print(dots)
-rMax = len(dots)
+rNum = len(dots)
+cNum = len(dots[0])
 playing = {}
 lastTime = 0
-for c in range(len(dots[0])):
+for c in range(cNum):
     first = True
-    for r in range(len(dots)):
-        if dots[r][c] != (playing[r] if r in playing else False):
-            if dots[r][c]:
-                addNoteMsg(True, rMax-r, length = 0.5*(c - lastTime), track = track)
-                lastTime = c
-                playing[r] = True
-            else:
-                addNoteMsg(False, rMax-r, length = 0.5*(c - lastTime) if first else 0, track = track)
+    if c == cNum-1:
+        for note in playing:
+            if playing[note]:
+                addNoteMsg(False, rNum-note, length = (c - lastTime) if first else 0, track = track)
                 lastTime = c
                 if first:
                     first = False
                 playing[r] = False
-first = True
-for note in playing:
-    if playing[note]:
-        addNoteMsg(False, rMax-note, length = 0.5*(c - lastTime) if first else 0, track = track)
-        lastTime = c
-        if first:
-            first = False
-        playing[r] = False
+    else:
+        for r in range(len(dots)):
+            if dots[r][c] != (playing[r] if r in playing else False):
+                if dots[r][c]:
+                    addNoteMsg(True, rNum-r, length = (c - lastTime), track = track)
+                    lastTime = c
+                    playing[r] = True
+                else:
+                    addNoteMsg(False, rNum-r, length = (c - lastTime) if first else 0, track = track)
+                    lastTime = c
+                    if first:
+                        first = False
+                    playing[r] = False
+
 midi.save(midiPath)
+flag = True
 if os.path.isfile(outputPath):
-    flag = False
     if 'yes' in args:
         flag = True
     elif 'no' in args:
@@ -124,8 +135,8 @@ if os.path.isfile(outputPath):
     else:
         flag = input('File \'{filePath}\' already exists. Overwrite? [y/N]'.format(filePath = outputPath))
         flag = not (flag.lower().find('n') > -1)
-    if flag:
-        copyFile(midiPath, outputPath)
+if flag:
+    copyFile(midiPath, outputPath)
 
 os.unlink(imagePath)
 os.unlink(midiPath)
